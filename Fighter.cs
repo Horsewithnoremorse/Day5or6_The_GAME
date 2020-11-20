e28f223fb7a05ebca61b4d78022fcb00cd8bbdae
Алексей Мильто <miltoalvit@gmail.com>
2020-11-20 21:34:41 +0300

    Добавьте файлы проекта.

---
+++ Fighter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Day5or6_The_GAME
+{
+    public class Fighter
+    {
+        public string Name;
+        public int Health;
+        public int Attack;
+        public int Strength;
+        Random random = new Random();
+        public Fighter(string n) 
+        { 
+            Name = n;
+            Health = random.Next(30) + 50 + 1;
+            Attack = random.Next(6) + 6 + 1;
+            Strength = random.Next(4) + 1;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {Name} Здоровье: {Health} Атака: {Attack} Сила: {Strength}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day5or6_The_GAME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[10];
+
+            string path = @"C:\Users\milto\source\repos\Day5or6_The_GAME\Names.txt";
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                int index = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    names[index] = line;
+                    Console.WriteLine(names[index]);
+                    index++;
+                }
+            }
+            List<Fighter> fighters = new List<Fighter>();
+
+            foreach (string nameFighter in names)
+            {
+                //nyashok.FoodsInFridge.Add(new Food() { Name = "Fish", Weight = 200, Price = 50 });
+                fighters.Add(new Fighter(nameFighter));
+            }
+            fighters[0].GetInfo;
+
+        }
+    }
+}
